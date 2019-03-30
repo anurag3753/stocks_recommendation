9e712a30f049f79a88e2235c51a5867838e987b0
Anurag Modi <anuragm@xlnx.xilinx.com>
2019-03-30 19:06:29 +0530

    Convert Class to function in update_database
    Signed-off-by: Anurag Modi <anuragm@xlnx.xilinx.com>

--- stocks_recommendation/src/update_database.py
+++ stocks_recommendation/src/update_database.py
@@ -1,10 +1,9 @@
 import datetime
 import pandas as pd
 
-from generic.utils import *
-from generic.stock_utils import *
-
-LAC = 100000
+from generic.stock_utils import process_stock_name, process_number
+from generic.stock_utils import get_db_connection, stocks_underwatch
+from generic.stock_utils import get_nth_date
 
 cols_needed = {
                 "Symbol":"symbol", 
@@ -15,10 +14,6 @@ cols_needed = {
                 "Traded Volume(lacs)":"volume"
                 }
 
-def process_volume(qty):
-    qty = process_number(qty)
-    return qty * LAC
-
 def get_dataframe(filepath, cols_needed):
     df = pd.read_csv(filepath)
     df.rename(columns = cols_needed, inplace = True)
@@ -27,7 +22,7 @@ def get_dataframe(filepath, cols_needed):
     df['high']   = df['high'].map(process_number)
     df['low']    = df['low'].map(process_number)
     df['close']  = df['close'].map(process_number)
-    df['volume'] = df['volume'].map(process_volume)
+    df['volume'] = df['volume'].map(process_number)
     mydf = df[cols_needed.values()]
     del df
     mydf['date'] = get_nth_date()
@@ -40,39 +35,32 @@ def get_stock_report(df, stock_name):
     except:
         return None
 
-class UpdateStockPrices:
-    def __init__(self, filepath):
-        self.filepath   = filepath
+def update_stock_prices(filepath):
+    # Connect to database
+    db_conn = get_db_connection()
+    engine  = db_conn.create_db_engine()
 
-    def update_stock_prices(self):
-        # Connect to database
-        db_conn = get_db_connection()
-        engine  = db_conn.create_db_engine()
+    # Get existing stocks
+    underwatch_stocks = stocks_underwatch()
 
-        # Get existing stocks
-        underwatch_stocks = stocks_underwatch()
+    # Read Filepath and get dataframe for the file
+    df = get_dataframe(filepath, cols_needed)
 
-        # Read Filepath and get dataframe for the file
-        df = get_dataframe(self.get_filepath(), cols_needed)
+    # Below stocks will be updated
+    print ("Following Stocks will be updated:")
+    print (underwatch_stocks)
 
-        # Below stocks will be updated
-        print ("Following Stocks will be updated:")
-        print (underwatch_stocks)
-
-        # Update the stock prices in database
-        for stock in underwatch_stocks:
-            data = get_stock_report(df, stock)
-            if data is not None:
-                try:
-                    data.to_sql(name=stock, con = engine, if_exists = 'append', index = False )
-                except Exception as e:
-                    msg = str(stock) + " " + "database updation failed"
-                    print(msg)
-                    print(e)
-                    pass
-
-    def get_filepath(self):
-        return self.filepath
+    # Update the stock prices in database
+    for stock in underwatch_stocks:
+        data = get_stock_report(df, stock)
+        if data is not None:
+            try:
+                data.to_sql(name=stock, con = engine, if_exists = 'append', index = False )
+            except Exception as e:
+                msg = str(stock) + " " + "database updation failed"
+                print(msg)
+                print(e)
+                pass
 
 if __name__ == "__main__":
     pass
