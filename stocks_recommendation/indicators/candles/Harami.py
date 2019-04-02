cf6b9381f236bf440ef41021b3114a88da39e112
Anurag Modi <anuragm@xlnx.xilinx.com>
2019-04-03 00:52:00 +0530

    Update Harami.py
    Signed-off-by: Anurag Modi <anuragm@xlnx.xilinx.com>

--- stocks_recommendation/indicators/candles/Harami.py
+++ stocks_recommendation/indicators/candles/Harami.py
@@ -1,10 +1,8 @@
 from indicators.candles.CandleTemplate import Candle
 from generic.stock_utils import general_info
-from generic.stock_utils import get_previous_day_stats
-from generic.stock_utils import is_uptrend
-from generic.stock_utils import is_downtrend
-from generic.stock_utils import is_bearish
-from generic.stock_utils import is_bullish
+from generic.stock_utils import get_previous_day_stats, get_latest_date_for_stock
+from generic.stock_utils import is_uptrend, is_downtrend
+from generic.stock_utils import is_bearish, is_bullish
 
 class Harami( Candle ):
     def __init__(self, o, h, l, c, stock_name, days = 5):
@@ -13,9 +11,10 @@ class Harami( Candle ):
         self.days = days
 
     def run(self):
-        po, ph, pl, pc, pv = get_previous_day_stats(self.stock_name)
+        date = get_latest_date_for_stock(self.stock_name)
+        po, ph, pl, pc, pv = get_previous_day_stats(self.stock_name, date=date)
 
-        if is_downtrend(stock_name = self.stock_name, days=self.days) and (is_bearish(po, pc)) and (self.o > pc) and is_bullish(self.o, self.c):
+        if is_downtrend(stock_name = self.stock_name, days=self.days) and (is_bearish(po, pc)) and (self.o > pc) and is_bullish(self.o, self.c) and (self.c < po):
             self.candle = True
             self.trade_setting["action"] = "buy"
             self.trade_setting["buy"] = self.c
@@ -24,7 +23,7 @@ class Harami( Candle ):
             self.trade_setting["target"] = ""
             self.trade_setting["info"]   = general_info("bullish_harami")
             return self.candle, self.trade_setting
-        elif is_uptrend(stock_name = self.stock_name, days=self.days) and (is_bullish(po, pc)) and (self.o < pc) and (self.c > po) and is_bearish(self.o, self.c):
+        elif is_uptrend(stock_name = self.stock_name, days=self.days) and (is_bullish(po, pc)) and (self.o < pc) and is_bearish(self.o, self.c) and (self.c > po):
             self.candle = True
             self.trade_setting["action"] = "short"
             self.trade_setting["buy"] = self.c
