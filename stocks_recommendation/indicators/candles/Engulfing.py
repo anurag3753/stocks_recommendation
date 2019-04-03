4b96a6374713525682396290e0e95b2c98895c17
Anurag Modi <anuragm@xlnx.xilinx.com>
2019-04-03 19:55:54 +0530

    Fix engulfing pattern
    Signed-off-by: Anurag Modi <anuragm@xlnx.xilinx.com>

--- stocks_recommendation/indicators/candles/Engulfing.py
+++ stocks_recommendation/indicators/candles/Engulfing.py
@@ -15,9 +15,15 @@ class Engulfing( Candle ):
         self.days = days
 
     def run(self):
-        po, ph, pl, pc, pv = get_previous_day_stats(self.stock_name)
+        date = get_latest_date_for_stock(self.stock_name)
+        po, ph, pl, pc, pv = get_previous_day_stats(self.stock_name, date=date)
+        downtrend = is_downtrend(stock_name = self.stock_name, days=self.days)
+        uptrend   = is_uptrend(stock_name = self.stock_name, days=self.days)
+        day1_range  = abs(po - pc)
+        day2_range  = abs(self.o - self.c)
+        day1_middle = float(day1_range)/2
 
-        if is_downtrend(stock_name = self.stock_name, days=self.days) and (is_bearish(po, pc)) and (self.c > po and self.o <= pc):
+        if downtrend and (is_bearish(po, pc)) and (is_bullish(self.o, self.c)) and (self.c > po and self.o <= pc):
             self.candle = True
             self.trade_setting["action"] = "buy"
             self.trade_setting["buy"] = self.c
@@ -27,7 +33,7 @@ class Engulfing( Candle ):
             self.trade_setting["info"]   = general_info("bullish_engulfing")
             return self.candle, self.trade_setting
         
-        elif is_uptrend(stock_name = self.stock_name, days=self.days) and (is_bullish(po, pc)) and (self.c < po and self.o > pc):
+        elif uptrend and (is_bullish(po, pc)) and (is_bearish(self.o, self.c)) and (self.c < po and self.o > pc):
             self.candle = True
             self.trade_setting["action"] = "short"
             self.trade_setting["buy"] = self.c
@@ -36,5 +42,25 @@ class Engulfing( Candle ):
             self.trade_setting["target"] = ""
             self.trade_setting["info"]   = general_info("bearish_engulfing")
             return self.candle, self.trade_setting
+
+        elif downtrend and (is_bearish(po, pc)) and (is_bullish(self.o, self.c)) and (day1_middle <= day2_range < day1_range):
+            self.candle = True
+            self.trade_setting["action"] = "buy"
+            self.trade_setting["buy"] = self.c
+            self.trade_setting["stoploss"] = max(self.l, pl)
+            self.trade_setting["candle"] = "piercing_pattern"
+            self.trade_setting["target"] = ""
+            self.trade_setting["info"]   = general_info("piercing_pattern")
+            return self.candle, self.trade_setting
+
+        elif uptrend and (is_bullish(po, pc)) and (is_bearish(self.o, self.c)) and (day1_middle <= day2_range < day1_range):
+            self.candle = True
+            self.trade_setting["action"] = "short"
+            self.trade_setting["buy"] = self.c
+            self.trade_setting["stoploss"] = max(self.h, ph)
+            self.trade_setting["candle"] = "dark_cloud_cover"
+            self.trade_setting["target"] = ""
+            self.trade_setting["info"]   = general_info("dark_cloud_cover")
+            return self.candle, self.trade_setting       
 
         return self.candle, self.trade_setting
