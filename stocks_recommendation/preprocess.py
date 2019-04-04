79b74db8c472870824f810ba64ae2b4fabeebaf7
Anurag Modi <anuragm@xlnx.xilinx.com>
2019-04-04 21:30:41 +0530

    Add pre-built for faster candle analysis
    Signed-off-by: Anurag Modi <anuragm@xlnx.xilinx.com>

---
+++ stocks_recommendation/preprocess.py
@@ -0,0 +1,38 @@
+import pickle
+import pandas as pd
+from setup import *
+from path import Path
+from generic.stock_utils import *
+from generic.utils import *
+
+'''Dictionary of Stocks list such that
+
+    stock[name]= {volume:[last_10_days], prev:[ohlc]}
+
+'''
+
+def generate_preprocessed_data():
+    stocks = list(stocks_underwatch())
+    prev_stats = dict()
+    result = dict()
+
+    for stock in stocks:
+        prev_stats[stock] = dict()
+
+        # Collect last 10 days volume average
+        prev_stats[stock]['vol'] = last_n_days_avg(stock, days=10, date = get_nth_date())
+
+        # Collect previous trend data
+        prev_stats[stock]['uptrend']   = is_uptrend(stock, date=get_nth_date(), days=5)
+        prev_stats[stock]['downtrend'] = is_downtrend(stock, date=get_nth_date(), days=5)
+
+        # Since we are running it, before updating the today's data in db
+        # So, techically previous day data, will be today's stats
+        prev_stats[stock]['p_ohlc'] = get_todays_stats(stock)
+
+        # Save the pre-processed data inside pickle
+        env = get_env(['pwd'])
+        filepath = env['pwd'] + '/' + 'pre-built' + '/' + 'processed_data.pickle'
+        save_to_pickle(prev_stats, filepath)
+
+generate_preprocessed_data()
